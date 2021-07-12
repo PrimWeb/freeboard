be17a0ef0b721bba4158a57f012657dd41623b0f
Rafael Vidal Aroca <rafaelaroca@gmail.com>
2021-07-12 07:47:13 -0300

    Disabled error messages

--- js/freeboard_plugins.js
+++ js/freeboard_plugins.js
@@ -2954,10 +2954,12 @@ function WidgetModel(theFreeboardModel, widgetPlugins) {
 				if (e instanceof ReferenceError && (/^\w+$/).test(rawValue)) {
 					returnValue = rawValue;
 				}
+				/*
 				if (showError) {
-					freeboard.showDialog(e, "Error with: " + settingName, "OK")
+					freeboard.showDialog(e, "xError with: " + settingName, "OK")
 					freeboard.playSound('error')
 				}
+				*/
 			}
 
 			var f = async function (returnValue) {
@@ -2982,7 +2984,7 @@ function WidgetModel(theFreeboardModel, widgetPlugins) {
 			catch (e) {
 				console.log(e.toString());
 				if (showError) {
-					freeboard.showDialog(e, "Error with: " + settingName, "OK")
+					freeboard.showDialog(e, "yError with: " + settingName, "OK")
 					freeboard.playSound('error')
 				}
 			}
@@ -3642,9 +3644,6 @@ var freeboard = (function () {
             'ubuntu-koto': 'ubuntu/Koto_chords.opus',
 			'ubuntu-mallet': 'ubuntu/Mallet.opus',
             'ubuntu': 'ubuntu/Ubuntu.opus'
-
-
-
 		},
 
 
@@ -8212,223 +8211,6 @@ wrench
 
   return $(x)
 }
-// # Building a Freeboard Plugin
-//
-// A freeboard plugin is simply a javascript file that is loaded into a web page after the main freeboard.js file is loaded.
-//
-// Let's get started with an example of a datasource plugin and a widget plugin.
-//
-// -------------------
-
-// Best to encapsulate your plugin in a closure, although not required.
-(function () {
-    // ## A Datasource Plugin
-    //
-    // -------------------
-    // ### Datasource Definition
-    //
-    // -------------------
-    // **freeboard.loadDatasourcePlugin(definition)** tells freeboard that we are giving it a datasource plugin. It expects an object with the following:
-    freeboard.loadDatasourcePlugin({
-        // **type_name** (required) : A unique name for this plugin. This name should be as unique as possible to avoid collisions with other plugins, and should follow naming conventions for javascript variable and function declarations.
-        "type_name": "core_customobj_plugin",
-        // **display_name** : The pretty name that will be used for display purposes for this plugin. If the name is not defined, type_name will be used instead.
-        "display_name": " Custom JS Object",
-        // **description** : A description of the plugin. This description will be displayed when the plugin is selected or within search results (in the future). The description may contain HTML if needed.
-        "description": "Create a custom object using JS code used as a constructor.  Standard freeboard event handlers work. Call notifyChange() to update dependant widgets.  Define a this.proxyHandler object to use all features of JS proxies.  Put your init code in onLoad()",
-        // **external_scripts** : Any external scripts that should be loaded before the plugin instance is created.
-
-        // **settings** : An array of settings that will be displayed for this plugin when the user adds it.
-        "settings": [
-
-            {
-                name: 'code',
-                type: 'constructor'
-            },
-
-            {
-                // **name** (required) : The name of the setting. This value will be used in your code to retrieve the value specified by the user. This should follow naming conventions for javascript variable and function declarations.
-                "name": "",
-                // **display_name** : The pretty name that will be shown to the user when they adjust this setting.
-                "display_name": "",
-                'html': "Show current internal state of object in JSON editor",
-
-                // **type** (required) : The type of input expected for this setting. "text" will display a single text box input. Examples of other types will follow in this documentation.
-                "type": "button",
-
-
-                // **description** : Text that will be displayed below the setting to give the user any extra information.
-                "description": "",
-
-                'onclick': function (n, i) {
-                    var x = []
-                    freeboard.showDialog($('<div id="fb-global-json-editor">'), "Debug data for: " + n + " (non-JSON not shown)", "OK", "Cancel",
-                        function () {
-                            Object.assign(i.proxy, x[0].getValue());
-                            x[0].destroy();
-                        },
-                        function () {
-                            x[0].destroy();
-                        }
-                    )
-
-                    var Editor = new JSONEditor(document.getElementById('fb-global-json-editor'), {
-                        schema: {}
-
-                    });
-                    x.push(Editor);
-                    Editor.setValue(i.data)
-
-
-
-                }
-            },
-            {
-                // **name** (required) : The name of the setting. This value will be used in your code to retrieve the value specified by the user. This should follow naming conventions for javascript variable and function declarations.
-                "name": "",
-                // **display_name** : The pretty name that will be shown to the user when they adjust this setting.
-                "display_name": "",
-                'html': "Show current state of object's exposed proxy in JSON editor",
-
-                // **type** (required) : The type of input expected for this setting. "text" will display a single text box input. Examples of other types will follow in this documentation.
-                "type": "button",
-
-
-                // **description** : Text that will be displayed below the setting to give the user any extra information.
-                "description": "",
-
-                'onclick': function (n, i) {
-                    var x = []
-                    freeboard.showDialog($('<div id="fb-global-json-editor">'), "Debug data for: " + n + " (non-JSON not shown)", "OK", "Cancel",
-                        function () {
-                            Object.assign(i.proxy, x[0].getValue());
-                            x[0].destroy();
-                        },
-                        function () {
-                            x[0].destroy();
-                        }
-                    )
-
-                    var Editor = new JSONEditor(document.getElementById('fb-global-json-editor'), {
-                        schema: {}
-
-                    });
-                    x.push(Editor);
-                    Editor.setValue(i.data)
-
-
-
-                }
-            }
-
-        ],
-        // **newInstance(settings, newInstanceCallback, updateCallback)** (required) : A function that will be called when a new instance of this plugin is requested.
-        // * **settings** : A javascript object with the initial settings set by the user. The names of the properties in the object will correspond to the setting names defined above.
-        // * **newInstanceCallback** : A callback function that you'll call when the new instance of the plugin is ready. This function expects a single argument, which is the new instance of your plugin object.
-        // * **updateCallback** : A callback function that you'll call if and when your datasource has an update for freeboard to recalculate. This function expects a single parameter which is a javascript object with the new, updated data. You should hold on to this reference and call it when needed.
-        newInstance: function (settings, newInstanceCallback, updateCallback) {
-            // myDatasourcePlugin is defined below.
-            newInstanceCallback(new myDatasourcePlugin(settings, updateCallback));
-        }
-    });
-
-
-    // ### Datasource Implementation
-    //
-    // -------------------
-    // Here we implement the actual datasource plugin. We pass in the settings and updateCallback.
-    var myDatasourcePlugin = function (settings, updateCallback) {
-        // Always a good idea...
-        var self = this;
-
-        // Good idea to create a variable to hold on to our settings, because they might change in the future. See below.
-        var currentSettings = settings;
-
-
-
-
-        function compile() {
-            currentSettings.code = currentSettings.code || ''
-            var notifyChange = function () {
-                updateCallback(self.proxy)
-            }
-
-            self.handler = {}
-
-            if (self.data) {
-                freeboard.unbindHandlers(self.data)
-            }
-            self.data = new (new Function("notifyChange", currentSettings.code))(notifyChange)
-
-            freeboard.bindHandlers(self.data)
-
-
-            self.proxy = new Proxy(self.data, self.data.proxyHandler || {})
-
-            if (self.data.onLoad) {
-                self.data.onLoad()
-            }
-            updateCallback(self.proxy)
-        }
-
-        compile()
-
-        self.compile = compile
-
-        /* This is some function where I'll get my data from somewhere */
-        function getData() {
-            var newData = self.proxy; // Just putting some sample data in for fun.
-
-            /* Get my data from somewhere and populate newData with it... Probably a JSON API or something. */
-            /* ... */
-
-            // I'm calling updateCallback to tell it I've got new data for it to munch on.
-            updateCallback(newData);
-        }
-
-
-
-        // **onSettingsChanged(newSettings)** (required) : A public function we must implement that will be called when a user makes a change to the settings.
-        self.onSettingsChanged = function (newSettings) {
-            var shouldRecompile = false
-            if (newSettings.code != currentSettings.code) {
-                shouldRecompile = true
-            }
-
-            // Here we update our current settings with the variable that is passed in.
-            currentSettings = newSettings;
-            if (shouldRecompile) {
-                self.compile()
-            }
-
-            updateCallback(self.proxy)
-        }
-        self.onCalculatedSettingChanged = function (k, v) {
-            if (k == 'data') {
-                self.data = v;
-                self.proxy = new Proxy(self.data, self.handler)
-                updateCallback(self.proxy);
-
-            }
-        }
-
-        // **updateNow()** (required) : A public function we must implement that will be called when the user wants to manually refresh the datasource
-        self.updateNow = function () {
-            // Most likely I'll just call getData() here.
-            getData();
-        }
-
-        // **onDispose()** (required) : A public function we must implement that will be called when this instance of this plugin is no longer needed. Do anything you need to cleanup after yourself here.
-        self.onDispose = function () {
-            if (self.data) {
-                freeboard.unbindHandlers(self.data)
-            }
-        }
-
-    }
-
-}());
-
 // ┌────────────────────────────────────────────────────────────────────┐ \\
 // │ freeboard-datagrid-plugin                                            │ \\
 // ├────────────────────────────────────────────────────────────────────┤ \\
@@ -11855,15 +11637,28 @@ freeboard.loadDatasourcePlugin({
             }
         }
 
+        this.updateMode = function(){
+            if(this.settings.mode=="cover")
+            {
+                $(widgetElement).css({
+                    width: "100%",
+                    height: "100%",
+                    "background-size": "cover",
+                    "background-position": "center"
+                });
+            }
+            else{
+                $(widgetElement).css({
+                    width: "100%",
+                    "background-size": "contain",
+                    "background-position": "center",
+                    "background-repeat":"no-repeat"
+                });
+            }
+        }
         this.render = function (element) {
-            $(element).css({
-                width: "100%",
-                height: "100%",
-                "background-size": "cover",
-                "background-position": "center"
-            });
-
             widgetElement = element;
+            this.updateMode()
         }
 
         this.onSettingsChanged = function (newSettings) {
@@ -11872,6 +11667,8 @@ freeboard.loadDatasourcePlugin({
             if (newSettings.refresh && newSettings.refresh > 0) {
                 timer = setInterval(updateImage, Number(newSettings.refresh) * 1000);
             }
+            this.settings = newSettings
+            this.updateMode()
         }
 
         this.onCalculatedValueChanged = function (settingName, newValue) {
@@ -11880,6 +11677,7 @@ freeboard.loadDatasourcePlugin({
             }
 
             updateImage();
+
         }
 
         this.onDispose = function () {
@@ -11904,12 +11702,29 @@ freeboard.loadDatasourcePlugin({
                 type: "calculated"
             },
             {
-                "type": "number",
+                "type": "calculated",
                 "display_name": "Refresh every",
                 "name": "refresh",
                 "suffix": "seconds",
                 "description": "Leave blank if the image doesn't need to be refreshed"
-            }
+            },
+
+            {
+                name: "mode",
+                display_name: "mode",
+                type: "option",
+                options: [
+                    {
+                        name: "Center",
+                        value: "center"
+                    },
+                    {
+                        name: "Cover",
+                        value: "cover"
+                    }
+                   
+                ]
+            },
         ],
         newInstance: function (settings, newInstanceCallback) {
             newInstanceCallback(new pictureWidget(settings));
